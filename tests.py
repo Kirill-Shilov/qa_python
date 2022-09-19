7eea29be92ca85d615272fd34b1cef06f297f4b2
Kirill Shilov <kirillvshilov@yandex.com>
2022-09-20 00:27:09 +0300

    few tests

--- tests.py
+++ tests.py
@@ -1,24 +1,36 @@
+import pytest
 from main import BooksCollector
 
+@pytest.fixture
+def collector():
+    return BooksCollector()
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
-
-        # добавляем две книги
+    def test_add_new_book_add_two_books(self, collector):
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_add_book_twise(self, collector):
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Гордость и предубеждение и зомби')
+
+        assert len(collector.get_books_rating()) == 1
+
+    def test_set_book_rating_set_rating_for_unexsists_book(self, collector):
+        collector.set_book_rating('unexists book', 4)
+        res = collector.get_book_rating('unexists book')
+
+        assert not res
+
+    def test_set_book_rating_set_rating_less_1(self, collector):
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_rating('Гордость и предубеждение и зомби', 0)
+        res = collector.get_book_rating('Гордость и предубеждение и зомби')
+
+        assert res != 0
+
+
